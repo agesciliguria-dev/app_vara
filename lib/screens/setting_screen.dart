ec424458528aaa1082273237e30364c8a5971ec7
TreRote42 <pietrimonta8@gmail.com>
2024-05-15 22:57:38 +0200

    added icon

--- lib/screens/setting_screen.dart
+++ lib/screens/setting_screen.dart
@@ -18,29 +18,13 @@ class SettingScreen extends StatefulWidget {
 }
 
 class _SettingScreenState extends State<SettingScreen> {
-  final webViewController = WebViewController()
-    ..loadRequest(Uri.parse(
-        'https://www.komoot.com/it-it/tour/1370054647/embed?profile=1&share_token=${constants.komootToken}'));
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Flutter WebView"),
-      ),
-      body: InAppWebView(
-        initialUrlRequest: URLRequest(
-          url: WebUri.uri(Uri.parse(
-              'https://www.komoot.com/it-it/tour/1370054647/embed?profile=1&share_token=${constants.komootToken}')),
-        ),
-        shouldOverrideUrlLoading: (controller, navigationAction) async {
-          if (!await launchUrl(Uri.parse(
-              "https://www.komoot.com/it-it/tour/1370054647?ref=wtd&share_token=${constants.komootToken}"))) {
-            throw Exception('Could not launch komoot');
-          }
-          return NavigationActionPolicy.CANCEL;
-        },
-      ),
+      )
     );
     //WebViewWidget(controller: webViewController));
   }
